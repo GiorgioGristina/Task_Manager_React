a4e2835dccbc8e101dd4325ed1942f1dd7b1bc1b
giorgio <giorgio.gristina91@gmail.com>
2025-02-11 15:37:57 +0000

    create sidebar componetn with button to start create new project and render in app componetn. created NewProject compoent the when button add project is clicked is diplayed. styled SideBar comnponetnt

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,23 @@
+import Input from "./Input";
+
+export default function NewProject(){
+    return (
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4 ">
+                <li>
+                    <button className="text-stone-700 hover:text-stone-950">Cancel</button>
+                </li>
+                <li>
+                    <button className="bg-blue-500 py-2 px-6 rounded-md hover:text-stone-700 ">Save</button>
+                </li>
+            </menu>   
+            <div>
+                <Input title="Title" />
+                <Input title="Description" textarea/>
+                <Input title="Due Date" />
+                
+            </div>             
+        </div>
+    );
+
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+export default function Sidebar({click}){
+    
+    function handleClick(){
+        click(true);
+        console.log(" the form has been open");
+    }
+    
+    return(
+
+        <aside className="h-full w-1/3 px-8 py-16 bg-blue-500 rounded-r-xl text-stone-50 md:w-72">
+            <h1 className="mb-8 font-bold">YOUR PROJECTS</h1>
+            <div>
+                <button onClick={handleClick} className="bg-stone-700 px-4 py-2 rounded-md hover:bg-stone-800 hover:text-stone-100">+ Add Project</button>    
+            </div>
+            <ul></ul>
+
+        </aside>
+    );
+}
